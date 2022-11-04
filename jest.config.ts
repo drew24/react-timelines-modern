42d036ba081ca3189edee1492a1724b9f852e458
Brandon Ramsey <brandonramsey@gmail.com>
2022-11-04 18:14:10 -0500

    Update test configuration to work with Typescript and new build process

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,14 @@
 import { Config } from 'jest';
 
 const config: Config = {
-  
-};
+  coveragePathIgnorePatterns: [ "/node_modules/" ],
+  moduleDirectories: [ "node_modules", "src" ],
+  preset: 'ts-jest',
+  resetMocks: true,
+  resetModules: true,
+  rootDir: "./src",
+  testEnvironment: 'node',
+  setupFilesAfterEnv: [ "../jestSetup.ts" ],
+}
 
 export default config;
