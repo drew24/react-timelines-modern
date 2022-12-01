b83ed681b7ab3aed3e12ea8d627083c3f148ef85
Joseph Hailu <joseph.hailu@contractor.evgo.com>
2022-12-01 10:28:10 -0500

    accept reactnode in trackkey

--- src/components/Sidebar/TrackKeys/types.ts
+++ src/components/Sidebar/TrackKeys/types.ts
@@ -1,7 +1,7 @@
-import { ReactHTMLElement } from "react";
+import { ReactHTMLElement, ReactNode } from "react";
 
 export interface Track<S extends HTMLElement = HTMLElement> {
-  title: string;
+  title: ReactNode;
   id: string;
   tracks?: Track[];
   isOpen?: boolean;
